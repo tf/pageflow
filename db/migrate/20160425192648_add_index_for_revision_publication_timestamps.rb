fd7111c252e70dd49a45ee190f52920b1f078bd3
Tim Fischbach <tfischbach@codevise.de>
2016-04-25 22:33:59 +0200

    fixup! Improve entry admin index query

--- db/migrate/20160425192648_add_index_for_revision_publication_timestamps.rb
+++ db/migrate/20160425192648_add_index_for_revision_publication_timestamps.rb
@@ -1,5 +1,5 @@
 class AddIndexForRevisionPublicationTimestamps < ActiveRecord::Migration
   def change
-    add_index 'pageflow_revisions', ['entry_id', 'published_at', 'published_until'], name: 'index_pageflow_revisions_on_entry_id_and_published_at_and_published_until', using: :btree
+    add_index 'pageflow_revisions', ['entry_id', 'published_at', 'published_until'], name: 'index_pageflow_revisions_on_publication_timestamps', using: :btree
   end
 end
