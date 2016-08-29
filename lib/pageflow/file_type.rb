fb3b82345561d3fd3ed7748729cb1437699ae313
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-08-29 11:57:02 +0200

    Adjust FileType
    Have it provide nested_file_types and make it work with BuiltInFileType.

--- lib/pageflow/file_type.rb
+++ lib/pageflow/file_type.rb
@@ -5,18 +5,14 @@ module Pageflow
     class NotFoundError < RuntimeError
     end
 
-    # ActiveRecord model that represents the files of this type.
-    # @return {ActiveRecord::Model}
-    attr_reader :model
-
     # Path of the partial used to render a json representation of the
     # file.
     # @return {String}
     attr_reader :editor_partial
 
-    # Underscored plural name for usage in routes.
-    # @return {String}
-    attr_reader :collection_name
+    # File types that are allowed to be nested inside the file
+    # @return {Array<FileType>}
+    attr_reader :nested_file_types
 
     # Create file type to be returned in {PageType#file_types}.
     #
@@ -26,17 +22,40 @@ module Pageflow
     #                          editor_partial: 'pageflow/rainbow/editor/files/file')
     #
     # @param [Hash] options
-    # @option options [ActiveRecord::Base] :model  Required. Model
-    #   representing the file.
+    # @option options [ActiveRecord::Base/String] :model  Required. Model
+    #   representing the file, or name of that model.
     # @option options [String] :editor_partial  Optional. Path of a
     #   partial to include in json representations of the file.
     # @option options [String] :collection_name  Optional. String to
     #   be used in routes. Defaults to `"pageflow_rainbow_file"` for
     #   model `Pageflow::Rainbow::File`.
+    # @option options [Array<FileType>] :nested_file_types
+    #   Optional. Array of FileTypes allowed for nested files. Defaults to [].
     def initialize(options)
-      @model = options.fetch(:model)
+      @model_string_or_reference = options.fetch(:model)
       @editor_partial = options.fetch(:editor_partial, nil)
-      @collection_name = options.fetch(:collection_name, model.model_name.plural)
+      @collection_name_or_blank = options.fetch(:collection_name, '')
+      @nested_file_types = options.fetch(:nested_file_types, [])
+    end
+
+    # ActiveRecord model that represents the files of this type.
+    # @return {ActiveRecord::Model}
+    def model
+      if @model_string_or_reference.is_a?(String)
+        @model_string_or_reference.constantize
+      else
+        @model_string_or_reference
+      end
+    end
+
+    # Underscored plural name for usage in routes.
+    # @return {String}
+    def collection_name
+      if @collection_name_or_blank.blank?
+        model.model_name.plural
+      else
+        @collection_name_or_blank
+      end
     end
 
     # @api private
@@ -55,6 +74,14 @@ module Pageflow
     # @api private
     def i18n_key
       model.model_name.i18n_key
+    end
+
+    # @api private
+    def supports_nested_file_type?(nested_model)
+      if nested_file_types.map(&:model).include?(nested_model)
+        return true
+      end
+      false
     end
   end
 end
