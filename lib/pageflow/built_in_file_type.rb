4b48420f6c5273c13457fcc63ffea9d03c86c794
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-09-01 17:59:39 +0200

    Add TextTrackFile
    Model, view, file type and migration.

--- lib/pageflow/built_in_file_type.rb
+++ lib/pageflow/built_in_file_type.rb
@@ -9,13 +9,21 @@ module Pageflow
     def self.video
       FileType.new(model: 'Pageflow::VideoFile',
                    editor_partial: 'pageflow/editor/video_files/video_file',
-                   collection_name: 'video_files')
+                   collection_name: 'video_files',
+                   nested_file_types: [BuiltInFileType.text_track])
     end
 
     def self.audio
       FileType.new(model: 'Pageflow::AudioFile',
                    editor_partial: 'pageflow/editor/audio_files/audio_file',
-                   collection_name: 'audio_files')
+                   collection_name: 'audio_files',
+                   nested_file_types: [BuiltInFileType.text_track])
+    end
+
+    def self.text_track
+      FileType.new(model: 'Pageflow::TextTrackFile',
+                   editor_partial: 'pageflow/editor/text_track_files/text_track_file',
+                   collection_name: 'text_track_files')
     end
   end
 end
