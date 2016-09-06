290ce17281ad2c544d17446c7c72783dbdfcd7bd
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-09-06 18:39:30 +0200

    Register text_track_files type
    Also,
    - add TextTrackFile model,
    - accept vtt filename ending,
    - add `default` option to TextTableCellView.

--- app/assets/javascripts/pageflow/editor/initializers/setup_file_types.js
+++ app/assets/javascripts/pageflow/editor/initializers/setup_file_types.js
@@ -8,13 +8,20 @@ pageflow.app.addInitializer(function(options) {
   pageflow.editor.fileTypes.register('video_files', {
     model: pageflow.VideoFile,
     metaDataAttributes: ['format', 'dimensions', 'duration'],
-    matchUpload: /^video/
+    matchUpload: /^video/,
+    settingsDialogTabConstructors: [pageflow.VideoSettingsTabView, pageflow.TextTrackTabView],
+    nestedFileTypes: ['text_track_files']
   });
 
   pageflow.editor.fileTypes.register('audio_files', {
     model: pageflow.AudioFile,
     metaDataAttributes: ['format', 'duration'],
     matchUpload: /^audio/
+  });
+
+  pageflow.editor.fileTypes.register('text_track_files', {
+    model: pageflow.TextTrackFile,
+    matchUpload: /vtt$/
   });
 
   pageflow.editor.fileTypes.setup(options.config.fileTypes);
