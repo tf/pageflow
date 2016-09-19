9f87513bd1a5364bdda23965cb202aa5de7edc0f
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-09-19 16:05:36 +0200

    13050 WIP

--- app/assets/javascripts/pageflow/editor/api.js
+++ app/assets/javascripts/pageflow/editor/api.js
@@ -37,6 +37,10 @@ pageflow.EditorApi = pageflow.Object.extend({
       this.sideBarRoutings.push(options);
     };
 
+    this.setUploadTargetFile = function(file) {
+      this.nextUploadTargetFile = file;
+    };
+
     /**
      * Set the name of the help entry that shall be selected by
      * default when the help view is opened. This value is
