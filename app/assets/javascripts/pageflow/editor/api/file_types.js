5f5b810ddef1532791105b470eaa1b42b1d47033
Tim Fischbach <mail@timfischbach.de>
2016-10-03 16:36:17 +0200

    Allow modifying registered file types
    Add `modify` method to `FileTypes` API, which can be used to register
    additional upload table columns, input form fields or filters for
    existing file types.

--- app/assets/javascripts/pageflow/editor/api/file_types.js
+++ app/assets/javascripts/pageflow/editor/api/file_types.js
@@ -1,6 +1,7 @@
 pageflow.FileTypes = pageflow.Object.extend({
   initialize: function() {
     this.clientSideConfigs = [];
+    this.clientSideConfigModifications = {};
   },
 
   register: function(name, config) {
@@ -11,19 +12,45 @@ pageflow.FileTypes = pageflow.Object.extend({
     this.clientSideConfigs[name] = config;
   },
 
+  modify: function(name, config) {
+    if (this._setup) {
+      throw 'File types already set up. Modify file types before initializers run.';
+    }
+
+    this.clientSideConfigModifications[name] = this.clientSideConfigModifications[name] || [];
+    this.clientSideConfigModifications[name].push(config);
+  },
+
   setup: function(serverSideConfigs) {
-    var clientSideConfigs = this.clientSideConfigs;
     this._setup = true;
 
     this.fileTypes = _.map(serverSideConfigs, function(serverSideConfig) {
-      var clientSideConfig = clientSideConfigs[serverSideConfig.collectionName];
+      var clientSideConfig = this.clientSideConfigs[serverSideConfig.collectionName];
 
       if (!clientSideConfig) {
         throw 'Missing client side config for file type "' + serverSideConfig.collectionName + '"';
       }
 
+      _(this.clientSideConfigModifications[serverSideConfig.collectionName])
+        .each(function(modification) {
+          clientSideConfig.configurationEditorInputs =
+            (clientSideConfig.configurationEditorInputs || []).concat(
+              modification.configurationEditorInputs || []
+            );
+
+          clientSideConfig.confirmUploadTableColumns =
+            (clientSideConfig.confirmUploadTableColumns || []).concat(
+              modification.confirmUploadTableColumns || []
+            );
+
+          clientSideConfig.filters  =
+            (clientSideConfig.filters || []).concat(
+              modification.filters || []
+            );
+        });
+
       return new pageflow.FileType(_.extend({}, serverSideConfig, clientSideConfig));
-    });
+    }, this);
   },
 
   findByUpload: function(upload) {
