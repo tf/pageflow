f73467c78c14fd07f74e967210fc45424c17de10
Tim Fischbach <tfischbach@codevise.de>
2016-10-20 15:00:43 +0200

    Port: file type fix

--- app/assets/javascripts/pageflow/editor/api/file_type.js
+++ app/assets/javascripts/pageflow/editor/api/file_type.js
@@ -8,7 +8,7 @@ pageflow.FileType = pageflow.Object.extend({
     this.i18nKey = options.i18nKey;
 
     this.confirmUploadTableColumns = options.confirmUploadTableColumns || [];
-    this.configurationEditorInputs = [].concat(options.configurationEditorInputs);
+    this.configurationEditorInputs = [].concat(options.configurationEditorInputs || []);
     this.configurationUpdaters = options.configurationUpdaters || [];
     this.metaDataAttributes = options.metaDataAttributes || [];
     this.filters = options.filters || [];
