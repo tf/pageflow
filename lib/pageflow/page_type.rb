7134c9cf1374db102e61026840f1a6e6ee2fa406
Tim Fischbach <tfischbach@codevise.de>
2016-12-28 15:09:37 +0100

    Introduce conditional thumbnail candidates
    Allow restricting when a thumbnail candidate may be used by giving an
    attribute and a desired value. This makes it possible, for example, to
    use different thumbnail candidates based on a `background_type`
    configuration attribute.

--- lib/pageflow/page_type.rb
+++ lib/pageflow/page_type.rb
@@ -104,12 +104,35 @@ module Pageflow
     # a registered {FileType}. The first file found is used as
     # thumbnail.
     #
-    # @example Default return value
-    #
     #     [
     #       {attribute: 'thumbnail_image_id', file_collection: 'image_files'},
     #       {attribute: 'background_image_id', file_collection: 'image_files'}
     #     ]
+    #
+    # It is possible to specify further conditions under which the
+    # candidate may be used via `if` or `unless` keys:
+    #
+    #     [
+    #       {
+    #         attribute: 'video_id',
+    #         file_collection: 'video_files',
+    #         if: {
+    #           attribute: 'background_type',
+    #           value: 'video',
+    #         }
+    #       },
+    #       {
+    #         attribute: 'image_id',
+    #         file_collection: 'image_files',
+    #         unless: {
+    #           attribute: 'background_type',
+    #           value: 'video',
+    #         }
+    #       }
+    #     ]
+    #
+    # The candidate will only be used if the given attribute has the
+    # given value.
     #
     # @returns {Array<Hash>}
     def thumbnail_candidates
