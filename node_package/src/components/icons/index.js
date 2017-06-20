cc947810fb48d13a0dd773ac30f8ce1582609358
Tim Fischbach <tfischbach@codevise.de>
2017-06-20 17:08:05 +0200

    Add WaveformPlayerControls component
    An alternative to the default player controls component based on
    wavesurfer.js.

--- node_package/src/components/icons/index.js
+++ node_package/src/components/icons/index.js
@@ -2,6 +2,8 @@ import Checkmark from './Checkmark';
 import CircleWithNotch from './CircleWithNotch';
 import Disk from './Disk';
 import Gear from './Gear';
+import Pause from './Pause';
+import Play from './Play';
 import Subtitles from './Subtitles';
 
 export default {
@@ -9,5 +11,7 @@ export default {
   CircleWithNotch,
   Disk,
   Gear,
+  Pause,
+  Play,
   Subtitles
 };
