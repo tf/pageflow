cb75d0f8b3910a240489229302397377e5652b12
Tim Fischbach <tfischbach@codevise.de>
2019-11-22 15:34:43 +0100

    Setup Jest for pageflow-react specs

---
+++ packages/pageflow-react/babel.config.js
@@ -0,0 +1,17 @@
+const presets = [
+  [
+    '@babel/env',
+    {
+      'corejs': 3,
+      'useBuiltIns': 'usage'
+    }
+  ],
+  '@babel/preset-react'
+];
+
+module.exports = {
+  presets,
+  exclude: [
+    'node_modules/**'
+  ]
+};
