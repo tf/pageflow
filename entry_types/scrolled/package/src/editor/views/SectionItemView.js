8b4f12b6c3ecee3f34e5217c3023e63cfee3fa95
Tim Fischbach <tfischbach@codevise.de>
2020-01-02 16:20:08 +0100

    Highlight active section/scroll to section

--- entry_types/scrolled/package/src/editor/views/SectionItemView.js
+++ entry_types/scrolled/package/src/editor/views/SectionItemView.js
@@ -1,20 +1,46 @@
 import Marionette from 'backbone.marionette';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {cssModulesUtils} from 'pageflow/ui';
 
 import {watchCollections} from '../../entryState';
 import {SectionThumbnail} from '../../frontend/SectionThumbnail'
 
+import styles from './SectionItemView.module.css';
+
 export const SectionItemView = Marionette.ItemView.extend({
   tagName: 'li',
+  className: styles.root,
 
   template: (data) => `
-    <a href="#/scrolled/sections/${data.id}">Section</a>
-    <div></div>
+    <div class="${styles.thumbnailContainer}">
+      <div class="${styles.thumbnail}"></div>
+      <div class="${styles.clickMask}"></div>
+    </div>
   `,
 
-  ui: {
-    thumbnail: 'div'
+  ui: cssModulesUtils.ui(styles, 'thumbnail'),
+
+  events: {
+    [`click .${styles.clickMask}`]: function() {
+      this.options.entry.trigger('scrollToSection', this.model);
+    }
+  },
+
+  initialize() {
+    this.listenTo(this.options.entry, 'change:currentSectionIndex', () => {
+      const active =
+        this.options.entry.sections.indexOf(this.model) === this.options.entry.get('currentSectionIndex');
+
+      this.$el.toggleClass(styles.active, active);
+
+      if (active) {
+        this.$el[0].scrollIntoView({
+          block: 'nearest',
+          behavior: 'smooth'
+        });
+      }
+    });
   },
 
   onRender() {
