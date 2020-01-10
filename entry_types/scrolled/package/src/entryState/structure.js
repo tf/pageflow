da575e7e3ab8f5b8e42bee997c8e3204fae9ca86
Tim Fischbach <tfischbach@codevise.de>
2020-01-10 15:26:46 +0100

    Add useSectionStructure hook
    Allow getting data for a single section.
    
    REDMINE-17298

---
+++ entry_types/scrolled/package/src/entryState/structure.js
@@ -0,0 +1,42 @@
+import {useMemo} from 'react';
+
+import {useEntryState} from './EntryStateProvider';
+import {getItems, getItem} from '../collections';
+
+export function useEntryStructure() {
+  const entryState = useEntryState();
+
+  return useMemo(() => {
+    return getItems(entryState.collections, 'chapters').map(chapter => ({
+      permaId: chapter.permaId,
+      ...chapter.configuration,
+      sections: getItems(entryState.collections, 'sections')
+        .filter(
+          item => item.chapterId === chapter.id
+        )
+        .map(section => sectionStructure(entryState.collections, section))
+    }));
+  }, [entryState]);
+};
+
+export function useSectionStructure({sectionPermaId}) {
+  const entryState = useEntryState();
+  const section = getItem(entryState.collections, 'sections', sectionPermaId)
+
+  return sectionStructure(entryState.collections, section);
+};
+
+function sectionStructure(collections, section) {
+  return section && {
+    ...section.configuration,
+    foreground: getItems(collections, 'contentElements')
+      .filter(
+        item => item.sectionId === section.id
+      )
+      .map(item => ({
+        type: item.typeName,
+        position: item.configuration.position,
+        props: item.configuration
+      }))
+  };
+}
