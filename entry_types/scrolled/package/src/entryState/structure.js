f4a0e8082cfb9e1b8ade4ff887b46d512436624a
Tim Fischbach <tfischbach@codevise.de>
2020-01-10 21:41:45 +0100

    WIP: Current section index fix

--- entry_types/scrolled/package/src/entryState/structure.js
+++ entry_types/scrolled/package/src/entryState/structure.js
@@ -7,15 +7,29 @@ export function useEntryStructure() {
   const entryState = useEntryState();
 
   return useMemo(() => {
-    return getItems(entryState.collections, 'chapters').map(chapter => ({
+    const sections = [];
+
+    const chapters = getItems(entryState.collections, 'chapters').map(chapter => ({
       permaId: chapter.permaId,
       ...chapter.configuration,
       sections: getItems(entryState.collections, 'sections')
         .filter(
           item => item.chapterId === chapter.id
         )
-        .map(section => sectionStructure(entryState.collections, section))
+        .map(section => {
+          const result = sectionStructure(entryState.collections, section);
+          sections.push(result);
+          return result;
+        })
     }));
+
+    sections.forEach((section, index) => {
+      section.sectionIndex = index;
+      section.previousSection = sections[index - 1];
+      section.nextSection = sections[index + 1];
+    });
+
+    return chapters;
   }, [entryState]);
 };
 
@@ -28,6 +42,7 @@ export function useSectionStructure({sectionPermaId}) {
 
 function sectionStructure(collections, section) {
   return section && {
+    permaId: section.permaId,
     ...section.configuration,
     foreground: getItems(collections, 'contentElements')
       .filter(
