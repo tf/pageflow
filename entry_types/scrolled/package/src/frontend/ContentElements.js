739633b9b970cc2d00084239b239d203e21c96b2
Tim Fischbach <tfischbach@codevise.de>
2020-02-19 14:57:41 +0100

    wip

--- entry_types/scrolled/package/src/frontend/ContentElements.js
+++ entry_types/scrolled/package/src/frontend/ContentElements.js
@@ -5,15 +5,18 @@ import {ContentElement} from './ContentElement';
 export function ContentElements(props) {
   return (
     <>
-      {props.items.map((item, index) =>
-        props.children(item,
+    {props.items.map((item, index) => {
+      if (!item.id) {
+        throw new Error('no key ' + JSON.stringify(item));
+      }
+      return props.children(item,
                        <ContentElement key={item.id}
                                        id={item.id}
                                        type={item.type}
                                        first={index === 0}
                                        position={item.position}
-                                       itemProps={item.props} />)
-       )}
+                                       itemProps={item.props} />);
+    })}
     </>
   );
 }
