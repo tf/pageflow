006ad5021b3b37be00be02771851afac25c3bd4e
Tim Fischbach <tfischbach@codevise.de>
2025-07-21 13:32:29 +0200

    wip

--- terragon-setup.sh
+++ terragon-setup.sh
@@ -3,11 +3,11 @@
 set -ex
 echo "Tesrt 12321"
 sudo add-apt-repository -y ppa:chris-needham/ppa
-sudo apt update
-sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev \
-                    libsqlite3-dev git curl libyaml-dev libgmp-dev autoconf \
-                    bison libffi-dev libgdbm-dev \
-                    mariadb-server libvips42 imagemagick audiowaveform
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev libreadline-dev zlib1g-dev \
+                        libsqlite3-dev git curl libyaml-dev libgmp-dev autoconf \
+                        bison libffi-dev libgdbm-dev \
+                        mariadb-server libvips42 imagemagick audiowaveform
 
 sudo service mariadb start
 mariadb --version
@@ -19,6 +19,10 @@ echo 'eval "$(rbenv init - bash)"' >> ~/.bashrc
 source ~/.bashrc
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+ls ~/.rbenv/bin
+echo $PATH
+which rbenv
 
 rbenv install 3.2.3
 rbenv global 3.2.3
