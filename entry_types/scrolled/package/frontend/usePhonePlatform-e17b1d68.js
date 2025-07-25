e77a581a87413c3b68e84a9752d39340c41ec2e1
Tim Fischbach <tfischbach@codevise.de>
2025-07-25 12:37:04 +0200

    Build

---
+++ entry_types/scrolled/package/frontend/usePhonePlatform-e17b1d68.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { P as PhonePlatformContext } from './PhonePlatformContext-71c3caf1.js';
+
+function usePhonePlatform() {
+  return React.useContext(PhonePlatformContext);
+}
+
+export { usePhonePlatform as u };
